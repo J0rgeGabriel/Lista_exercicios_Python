05bc0c04f0490de0ecb029d0a3d03af922f1f486
Jorge Gabriel <127042883+J0rgeGabriel@users.noreply.github.com>
2023-06-03 14:43:15 -0300

    update de exercicios feitos

--- Estrutura_decisao.py
+++ Estrutura_decisao.py
@@ -116,18 +116,110 @@ while True:
 
 salario = float(input("Informe o seu salario: R$").replace(",", "."))
 if salario <= 280:
-    salario_aumento = salario * 1.2
+    porcentagem = 1.2
+    salario_aumento = salario * porcentagem
     print(
         f"Após o aumento de 20%, o seu salario será de R${salario_aumento:.2f}")
 elif salario > 280 and salario <= 700:
-    salario_aumento = salario * 1.15
+    porcentagem = 1.15
+    salario_aumento = salario * porcentagem
     print(
         f"Após o aumento de 15%, o seu salario será de R${salario_aumento:.2f}")
 elif salario > 700 and salario < 1500:
-    salario_aumento = salario * 1.10
+    porcentagem = 1.10
+    salario_aumento = salario * porcentagem
     print(
         f"Após o aumento de 10%, o seu salario será de R${salario_aumento:.2f}")
 else:
-    salario_aumento = salario * 1.05
+    porcentagem = 1.05
+    salario_aumento = salario * porcentagem
     print(
         f"Após o aumento de 5%, o seu salario será de R${salario_aumento:.2f}")
+        
+print(f"O seu salario antes do reajuste era R${salario:.2f}")
+print(f"O percentual do aumento foi de {porcentagem:.2f}%")
+print(f"O seu novo salário será de R${salario_aumento:.2f}")
+
+# 12. Faça um programa para o cálculo de uma folha de pagamento, sabendo que os descontos são do Imposto de Renda, que
+# depende do salário bruto (conforme tabela abaixo) e 3% para o Sindicato e que o FGTS corresponde a 11% do Salário
+# Bruto, mas não é descontado (é a empresa que deposita). O Salário Líquido corresponde ao Salário Bruto menos os
+# descontos. O programa deverá pedir ao usuário o valor da sua hora e a quantidade de horas trabalhadas no mês.
+# Desconto do IR:
+# • Salário Bruto até 900 (inclusive) - isento
+# • Salário Bruto até 1500 (inclusive) - desconto de 5%
+# • Salário Bruto até 2500 (inclusive) - desconto de 10%
+# Salário Bruto acima de 2500 - desconto de 20% Imprima na tela as informações, dispostas conforme o exemplo
+# abaixo. No exemplo o valor da hora é 5 e a quantidade de hora é 220.
+
+salario_bruto = float(input("Informe o seu salario bruto: R$").replace(",", "."))
+imposto_renda = 0
+porcentagem = 0
+if salario_bruto <= 900:
+    print("Você está isento do imposto de renda")
+elif salario_bruto > 900 and salario_bruto <= 1500:
+    porcentagem = 0.05
+    imposto_renda = salario_bruto * porcentagem 
+    print(f"Será descontado 5% do seu salário bruto referente ao imposto de renda, isso dará R${imposto_renda}")
+elif salario_bruto > 1500 and salario_bruto <= 2500:
+    porcentagem = 0.10
+    imposto_renda = salario_bruto * porcentagem 
+    print(f"Será descontado 10% do seu salário bruto referente ao imposto de renda, isso dará R${imposto_renda}")
+else:
+    porcentagem = 0.20
+    imposto_renda = salario_bruto * porcentagem 
+    print(f"Será descontado 20% do seu salário bruto referente ao imposto de renda, isso dará R${imposto_renda}")
+
+FGTS = salario_bruto * 0.11
+INSS = salario_bruto * 0.10
+sindicato = salario_bruto * 0.03
+salarioliquido = salario_bruto - imposto_renda - INSS - sindicato
+total_desconto = INSS + sindicato + imposto_renda
+
+print(f"O seu salário bruto será de R${salario_bruto:.2f}")
+print(f"Será descontado R${INSS:.2f} (10%) referente ao INSS")
+print(f"Será descontado R${sindicato:.2f} (3%) referente ao sindicato")
+print(f"Será depositado R${FGTS:.2f} (11%) referente ao FGTS")
+print(f"O total de desconto é de R${total_desconto:.2f}")
+print(f"O seu salário será de R${salarioliquido:.2f}")
+
+# 13. Faça um Programa que leia um número e exiba o dia correspondente da semana. (1-Domingo, 2- Segunda, etc.), se digitar
+# outro valor deve aparecer valor inválido.
+while True:
+    dia_semana = input("Digite um número referente ao dia da semana:\n"
+                    "1 - Domingo\n"
+                    "2 - Segunda-feira\n"
+                    "3 - Terça-feira\n"
+                    "4 - Quarta-feira\n"
+                    "5 - Quinta-feira\n"
+                    "6 - Sexta-feira\n"
+                    "7 - Sábado\n"
+                    "0 - Parar o programa")
+    print()
+    if dia_semana == "1":
+        print("Domingo")
+    elif dia_semana == "2":
+        print("Segunda-feira")
+    elif dia_semana == "3":
+        print("Terça-feira")
+    elif dia_semana == "4":
+        print("Quarta-feira")
+    elif dia_semana == "5":
+        print("Quinta-feira")
+    elif dia_semana == "6":
+        print("Sexta-feira")
+    elif dia_semana == "7":
+        print("Sabado")
+    elif dia_semana == "0":
+        print("Programa encerrado...")
+        break
+    else:
+        dia_semana = input("Valor invalido!! Digite um número referente ao dia da semana:\n"
+                    "1 - Domingo\n"
+                    "2 - Segunda-feira\n"
+                    "3 - Terça-feira\n"
+                    "4 - Quarta-feira\n"
+                    "5 - Quinta-feira\n"
+                    "6 - Sexta-feira\n"
+                    "7 - Sábado\n")
+        print()
+        continue
